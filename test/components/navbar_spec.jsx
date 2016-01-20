f0f2b605afdfe95c9fb00696ad2d3bdbaf7f3b82
Andrew Bethel <andrew@striking.ly>
2016-01-20 13:28:13 -0600

    navbar test suite

---
+++ test/components/navbar_spec.jsx
@@ -0,0 +1,25 @@
+import React from 'react/addons'
+import {Navbar} from '../../src/app/components/Navbar'
+import {expect} from 'chai'
+
+const {renderIntoDocument, scryRenderedDOMComponentsWithTag, scryRenderedDOMComponentsWithClass, Simulate} = React.addons.TestUtils
+
+describe('Navbar', () => {
+  it('Does not render nav options without auth', () => {
+    const component = renderIntoDocument(
+      <Navbar auth={false} />
+    )
+    const navbarList = scryRenderedDOMComponentsWithClass(component, 'nav-options')
+
+    expect(navbarList.length).to.equal(0)
+  })
+
+  it('Shows nav options with auth', () => {
+    const component = renderIntoDocument(
+      <Navbar auth={true} />
+    )
+    const navbarList = scryRenderedDOMComponentsWithClass(component, 'nav-options')
+
+    expect(navbarList.length).to.equal(1)
+  })
+})
